4b1cedd0d7e78e3cb4fb63ceafe2634112242f44
zabi <javazshk@gmail.com>
2022-01-29 16:46:35 -0500

    refactor code and adding metadata.

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Home } from './pages';
 // import SigninPage from './pages/signin';
 import PageNotFound from './components/PageNotFound';
+import { Helmet } from 'react-helmet';
 
 function App() {
 
   useEffect(() => {
-    document.title = "TechAlchemy, LLC"
+    document.title = "TechAlchemy, LLC.,"
   }, []);
   
   return (
     <>
+    <Helmet>
+    <title>TechAlchemy LLC.</title>
+        <meta name="description" content="TechAlchemy is a leading IT firm providing innovative software solutions." />
+        <meta name="theme-color" content="#00897B" />
+    </Helmet>
       <Router>
         <Switch>
           <Route path="/" component={Home} exact/>
